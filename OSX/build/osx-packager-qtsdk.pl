a2f9a0201610fc18bacdc5e1a84c50ddbd1a2bf4
David Hampton <mythtv@love2code.net>
2019-01-08 10:19:45 -0500

    Misunderstood the script structure.

--- OSX/build/osx-packager-qtsdk.pl
+++ OSX/build/osx-packager-qtsdk.pl
@@ -980,9 +980,9 @@ EOF
     {
         'url' => "https://download.qt.io/archive/qt/5.5/5.5.1/single/qt-everywhere-opensource-src-5.5.1.tar.xz",
         'pre-conf'
-        => "cd qtbase; sed -i.bak -e 's/xcrun -find xcrun/xcrun -find xcodebuild/' configure"
-        => "cd qtbase/mkspecs/features/mac; sed -i.bak -e 's/xcrun -find xcrun/xcrun -find xcodebuild/' default_pre.prf"
-        => "cd qtbase/src/plugins/sqldrivers/mysql; cp mysql.pro mysql2.pro ; " .
+        => "sed -i.bak -e 's/xcrun -find xcrun/xcrun -find xcodebuild/' qtbase/configure; "
+           "sed -i.bak -e 's/xcrun -find xcrun/xcrun -find xcodebuild/' qtbase/mkspecs/features/mac/default_pre.prf; "
+           "cd qtbase/src/plugins/sqldrivers/mysql; cp mysql.pro mysql2.pro ; " .
             "echo \"target.path=$PREFIX/qtplugins-$QTVERSION\" >> mysql2.pro; " .
             "echo \"INSTALL += target\" >> mysql2.pro",
         'conf-cmd'
